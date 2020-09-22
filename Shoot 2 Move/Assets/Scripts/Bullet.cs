a4a480ee91afebc011a59531b3a226c8b11f1709
Vaisakh Chandran B <59969608+vcb2020@users.noreply.github.com>
2020-09-22 23:18:27 +0530

    Base Game
    base mechanics with 0 addons

---
+++ Shoot 2 Move/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //public GameObject hiteffect;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hiteffect, transform.position, Quaternion.identity);
+        // Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
+  
---
+++ Shoot 2 Move/Assets/ShootAndMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAndMove : MonoBehaviour
+{
+    //public input
+    public Rigidbody2D player;
+    public Camera cam;
+    public float moveSpeed = 5f;
+    public Transform firePoint;
+    public Transform movePoint;
+    public GameObject BulletPrefab;
+    public float bulletForce = 20f;
+
+    Vector2 movement, mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            player.AddForce(movePoint.up * moveSpeed, ForceMode2D.Impulse);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        //player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - player.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        player.rotation = angle;
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        //Debug.Log("fire");
+    }
+}
