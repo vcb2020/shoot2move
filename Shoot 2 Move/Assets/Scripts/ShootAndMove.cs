bbcad74efac167b428b5de45922838c478f4612c
Vaisakh Chandran B <59969608+vcb2020@users.noreply.github.com>
2020-09-24 18:39:17 +0530

    PUSH

--- Shoot 2 Move/Assets/Scripts/ShootAndMove.cs
+++ Shoot 2 Move/Assets/Scripts/ShootAndMove.cs
@@ -18,19 +18,18 @@ public class ShootAndMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //movement.x = Input.GetAxisRaw("Horizontal");
-        //movement.y = Input.GetAxisRaw("Vertical");
+        
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
-            player.AddForce(movePoint.up * moveSpeed, ForceMode2D.Impulse);
+            MovePlayer();
         }
 
     }
     private void FixedUpdate()
     {
-        //player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
         Vector2 lookDir = mousePos - player.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         player.rotation = angle;
@@ -40,6 +39,9 @@ public class ShootAndMove : MonoBehaviour
         GameObject bullet = Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-        //Debug.Log("fire");
+    }
+    void MovePlayer()
+    {
+        player.AddForce(movePoint.up * moveSpeed, ForceMode2D.Impulse);
     }
 }
